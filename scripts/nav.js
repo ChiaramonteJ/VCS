a84a9e6f89f61db05adb8caa229559dc4d6af541
ChiaramonteJ <chiaramonte07@icloud.com>
2024-12-12 19:21:40 -0500

    Major changes and updates to all pages. added images and blog

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,14 +1,14 @@
 // scripts/nav.js
 document.addEventListener("DOMContentLoaded", () => {
     // Load Header
-    fetch("./components/header.html")
+    fetch("/components/header.html")
       .then((response) => response.text())
       .then((data) => {
         document.querySelector("header").innerHTML = data;
       });
   
     // Load Footer
-    fetch("./components/footer.html")
+    fetch("/components/footer.html")
       .then((response) => response.text())
       .then((data) => {
         document.querySelector("footer").innerHTML = data;
